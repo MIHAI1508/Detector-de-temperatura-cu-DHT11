4fd5e9b42c2a0f9f175a26e8134653d6c0b095da
MIHAI1508 <81927122+MIHAI1508@users.noreply.github.com>
2024-03-14 22:25:05 +0200

    Update Proiect_PM.ino

--- Proiect_PM.ino
+++ Proiect_PM.ino
@@ -1,33 +1,52 @@
 
 #include  <DHT.h> ;
 #include  <LiquidCrystal_I2C.h> ;
-#include <Wire.h> ;
+#include  <Wire.h> ;
 
-//Constants
 #define  DHTPIN 7   
 #define DHTTYPE DHT11
 #define R_LED 6
 #define G_LED 5
-LiquidCrystal_I2C lcd(0x27,16,2);  
+
 DHT dht(DHTPIN, DHTTYPE);
 
-//Variables
+LiquidCrystal_I2C lcd(0x27,20,4);  
+
+
 int chk;
 float hum;  
 float temp; 
-const int TEMP_THRESHOLD_UPPER = 35; // upper threshold of temperature, change to your desire value
-const int TEMP_THRESHOLD_LOWER = 35; // lower threshold of temperature, change to your desire value
+const int TEMP_THRESHOLD_UPPER = 35; 
+const int TEMP_THRESHOLD_LOWER = 35; 
+
+
+byte grade[8] ={0b00111,0b00101,0b00111,0b00000,0b00000,0b00000,0b00000,0b00000};
+
+byte Led_OFF_ST[8] ={0b00000,0b00011,0b00100,0b00100,0b00100,0b01111,0b00010,0b00010};
+
+byte Led_OFF_DR[8] ={0b00000,0b11000,0b00100,0b00100,0b00100,0b11110,0b01000,0b01000};
+
+byte Led_ON_ST[8] ={0b00000,0b00011,0b00111,0b00111,0b00111,0b01111,0b00010,0b00010};
+
+byte Led_ON_DR[8] ={0b00000,0b11000,0b11100,0b11100,0b11100,0b11110,0b01000,0b01000};
+
 
 void setup()
 {
-    Serial.begin(9600);
-    lcd.init();
-    dht.begin();                       
-  // Print a message to the LCD.
+  Serial.begin(9600);
+  lcd.init();
+  lcd.begin(16,4);               
+  dht.begin();                       
   lcd.backlight();
   lcd.setBacklight(HIGH);
+  
   pinMode(R_LED, OUTPUT);
   pinMode(G_LED, OUTPUT);
+  lcd.createChar(1, grade);
+  lcd.createChar(2, Led_OFF_ST);
+  lcd.createChar(3, Led_OFF_DR);
+  lcd.createChar(4, Led_ON_ST);
+  lcd.createChar(5, Led_ON_DR);
 }
 
 void  loop()
@@ -37,38 +56,58 @@ void  loop()
     hum = dht.readHumidity();
     temp= dht.readTemperature();
 
-    //Print temp and humidity values to LCD
-
-    lcd.setCursor(0,0);
-    lcd.print("Humidity:  ");
+    lcd.setCursor(3,0);
+    lcd.print("HUM:");
     lcd.print(hum);
     lcd.print("%");
-    lcd.setCursor(0,1);
-    lcd.print("Temp: "); 
+    lcd.setCursor(2,1);
+    lcd.print("Temp:"); 
     lcd.print(temp);
-    lcd.println(" C");
+    lcd.write(byte(1));
+    lcd.print("C");
 
-    delay(100); //Delay 2 sec between temperature/humidity check.
 
-    Serial.print("Humidity: ");
-    Serial.print(hum);
-    Serial.print(" %, Temp: ");
-    Serial.print(temp);
-    Serial.println(" C");
 
-    delay(100); //Delay 2 sec.
+    delay(10); 
 
-    if (isnan(temp)) {
-    Serial.println("Failed to read from DHT sensor!");
-  } else {
-    if(temp > TEMP_THRESHOLD_UPPER){
-      Serial.println("The relay is turned on");
+
+    if(temp < TEMP_THRESHOLD_UPPER){
+    lcd.setCursor(0,2);
+    lcd.print("G");
+    lcd.write(byte(4));
+    lcd.write(byte(5));
+
+    lcd.setCursor(5,2);
+    lcd.print("R");
+    lcd.write(byte(2));
+    lcd.write(byte(3));
+    } 
+    else if(temp >= TEMP_THRESHOLD_LOWER){
+    lcd.setCursor(0,2);
+    lcd.print("G");
+    lcd.write(byte(2));
+    lcd.write(byte(3));
+
+    lcd.setCursor(5,2);
+    lcd.print("R");
+    lcd.write(byte(4));
+    lcd.write(byte(5));
+    }
+
+    delay(10); 
+    
+
+
+    if(temp >= TEMP_THRESHOLD_UPPER){
+      Serial.println("Red_led_High");
       digitalWrite(R_LED, HIGH);
       digitalWrite(G_LED, LOW); 
     } else if(temp < TEMP_THRESHOLD_LOWER){
-      Serial.println("The relay is turned off");
+      Serial.println("Green_led_High");
       digitalWrite(R_LED, LOW);  
       digitalWrite(G_LED, HIGH); 
     }
-  }
+    
+    lcd.setCursor(-2, 3);
+    lcd.print("Manta Mihai");
 }
